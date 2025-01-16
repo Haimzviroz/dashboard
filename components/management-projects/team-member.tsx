308db475aed15f944908d20b8170804367284d33
David Klein <davidk@ravtech.co.il>
2025-01-16 14:39:59 +0200

    refactor: consolidate team member components and improve status label handling

--- components/management-projects/team-member.tsx
+++ components/management-projects/team-member.tsx
@@ -1,83 +1,109 @@
-import { FC, Fragment, useState } from "react"
+import { Member } from "@/types/interfaces";
+import { Box, Avatar, Stack, Typography, IconButton, Chip } from "@mui/material";
+import { Delete as DeleteIcon, Group as GroupIcon, Edit as EditIcon } from "@mui/icons-material";
 
-import { deleteMember, updateMember } from "@/apis/client-side/projects-actions.api"
 
-import MemberForm from "./member-from"
-
-import { Project, Member } from '@/types/interfaces';
-
-import s from '../../styles/management-project.module.css';
-
-interface TeamMemberProps {
-  member: Member,
-  project: Project,
-  setProject: (project: any) => void
+interface TeamMemberItemProps {
+  member: Member;
+  onEdit: () => void;
+  onDelete: () => void;
 }
 
-const TeamMember: FC<TeamMemberProps> = ({ member, project, setProject }) => {
-  const [doDisplayActions, setDoDisplayActions] = useState<boolean>(false)
-  const [doEditMember, setDoEditMember] = useState<boolean>(false)
+const TeamMemberItem: React.FC<TeamMemberItemProps> = ({
+  member,
+  onEdit,
+  onDelete,
+}) => {
 
-  const submitUpdatedMember = async (data: any) => {
-    data = { ...data, projectId: project.id }
-    try {
-      const res = await updateMember(project.id, member.id, data)
-      const memberIndex = project.members.findIndex((m: any) => m.id === member.id)
-      project.members.splice(memberIndex, 1, res)
-      const updatedProject = { ...project }
-      setProject(updatedProject)
-      setDoDisplayActions(!doDisplayActions)
-      setDoEditMember(false)
-    } catch (error) {
-      console.error(error)
+  const StatusLabel = (label: string) => {
+    let bgColor;
+    switch (label) {
+      case "active":
+        bgColor = "#4CAF50";
+        break;
+      case "inactive":
+        bgColor = "#9E9E9E";
+        break;
+      case "invited":
+        bgColor = "#2196F3";
+        break;
+      default:
+        bgColor = "#4CAF50";
+        break;
     }
-  }
 
-  const handleDeleteMember = async () => {
-    const doDeleteMember = confirm(`Are you sure that you want delete ${member.firstName} ${member.lastName} `)
-    if (doDeleteMember) {
-      const data = { email: member.email }
+    return (
+      <Chip
+        label={label}
+        sx={{
+          backgroundColor: bgColor,
+          color: "white",
+          height: "16px",
+          fontSize: "12px",
+          fontWeight: "normal",
+          marginTop: 0.5,
+        }}
+      />
+    );
+  };
 
-      try {
-        await deleteMember(project.id, member.id, data)
-        const memberIndex = project.members.findIndex((m: any) => m.id === member.id)
-        project.members.splice(memberIndex, 1)
-        const updatedProject = { ...project }
-        setProject(updatedProject)
-      } catch (error) {
-
-      }
+  function stringToColor(string: string) {
+    let hash = 0;
+    for (let i = 0; i < string.length; i++) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
+    let color = "#";
+    for (let i = 0; i < 3; i++) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    return color;
   }
 
   return (
-    <Fragment>
-      <div
-        onMouseEnter={() => { setDoDisplayActions(true) }}
-        onMouseLeave={() => { setDoDisplayActions(false) }}
-        className={s["item-wrap"]}
-      >
-        <div className={`${s.row}`}>
-          <div className={s["member-name-wrap"]}>
-            <div>{`${member.firstName?? ""} ${member.lastName?? ""} ${(!member.firstName && !member.lastName) ? member.email : ""}`} </div>
-          </div>
-          {
-            doDisplayActions &&
-            <div>
-              <button className={`${s["edit-button"]} ${s.button}`} onClick={() => { setDoEditMember(!doEditMember) }} >Edit</button>
-              <button className={`${s["delete-button"]} ${s.button}`} onClick={() => { handleDeleteMember() }}>Delete</button>
-            </div>
-          }
-        </div>
-        {
-          doEditMember &&
-          <MemberForm submit={submitUpdatedMember} member={member} ></MemberForm>
-        }
-      </div>
-    </Fragment>
-  )
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      padding={1}
+      borderRadius={1}
+      sx={{ backgroundColor: "#f9f9f9" }}
+    >
+      <Box display="flex" alignItems="center" gap={1}>
+        <Avatar
+          src={`${member.image}`}
+          alt={`${member.firstName} ${member.lastName}`}
+          sx={{
+            width: 48,
+            height: 48,
+            marginRight: 2,
+            bgcolor: !member.image
+              ? stringToColor(`${member.firstName} ${member.lastName}`)
+              : undefined,
+          }}
+        />
+        <Box>
+          <Stack direction={"row"} gap={1}>
+            <Typography variant="body1" fontWeight="bold">
+              {`${member.firstName} ${member.lastName}`}
+            </Typography>
+            {StatusLabel(member.status)}
+          </Stack>
+          <Typography variant="body2" color="text.secondary">
+            {member.role}
+          </Typography>
+        </Box>
+      </Box>
+      <Box>
+        <IconButton onClick={onEdit} aria-label="edit">
+          <EditIcon />
+        </IconButton>
+        <IconButton onClick={onDelete} color="error" aria-label="delete">
+          <DeleteIcon />
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
 
-
-}
-
-export default TeamMember
+export default TeamMemberItem
