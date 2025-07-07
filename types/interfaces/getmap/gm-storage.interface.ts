e0279f42f6a477fd96c439c5fb57c18028a197c1
David Klein <davidk@ravtech.co.il>
2025-07-07 13:38:06 +0300

    feat: Refactor GetAppProvider by removing sidebar state management and related props

--- types/interfaces/getmap/gm-storage.interface.ts
+++ types/interfaces/getmap/gm-storage.interface.ts
@@ -4,17 +4,8 @@ import { NextRouter } from "next/router";
 
 export interface GetAppProviderProps {
   router: NextRouter
-  activated: Activate | null
-  setActivated: Dispatch<SetStateAction<Activate | null>>;
   proNavBarActive: ProNavBarOption | null
   setProNavBarActive: Dispatch<SetStateAction<ProNavBarOption | null>>;
   dashNavBarActive: DashNavBarOption | null
   setDashNavBarActive: Dispatch<SetStateAction<DashNavBarOption | null>>;
-  sideBarCollapse: boolean
-  setSideBarCollapse: Dispatch<SetStateAction<boolean>>;
-}
-
-export interface Activate {
-  active: SideBarOption
-  trigger: "sideBar" | null
 }
